31479d1ac04a663d4ffd4e678ab3cadd08b911f1
Rafael Martinez <rrhhmmtt@gmail.com>
2023-02-27 19:07:55 -0500

    se crea estable router con gets

--- src/server/server.js
+++ src/server/server.js
@@ -1,34 +1,14 @@
 import express from "express";
-import ProductManager from "../app/productManager.js";
 import productManagerRouter from "../routes/productManager.router.js";
+import __dirname from "./utils.js";
 
-const item = new ProductManager();
 const app = express();
+app.use(express.json());
+app.use(express.static(__dirname + "../../public"));
 
-app.use(`/api/products`, productManagerRouter);
-
-//params
-
-//lista de max 3 items
-app.get("/products", async (req,res) => {
-    const {limit} = req.query;
-    const prods = await item.getProducts(); 
-    if(!limit){
-       await res.send(prods);
-       console.log("se muestra todo");
-    }
-    //envia el filtrado de el numero de datos
-    const filtered = prods.splice(0,limit);
-    console.log("se muestra limitado");
-    await res.send(filtered);
-});
-
-//segun el id
-app.get("/products/:id", async (req,res) => {
-    const prodId = await Number(req.params.id);
-    const result = await item.getProductById(prodId);
-    await res.send(result);
-})
+//products route
+app.use("/api/products", productManagerRouter);
+//cart route
 
 //escucha
 app.listen(8080, () => {
