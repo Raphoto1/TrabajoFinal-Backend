396c000e882c6e1362fa4b57c0e0952747576f85
Rafael Martinez <rrhhmmtt@gmail.com>
2023-03-02 22:47:27 -0500

    se agregan rutas para postman en server.js

--- src/server/server.js
+++ src/server/server.js
@@ -15,3 +15,27 @@ app.use("/api/cart", cartManagerRouter)
 app.listen(8080, () => {
 console.log("listening 8080");
 })
+
+//get products
+// localhost:8080/api/products
+
+//get products by id
+// localhost:8080/api/products/2
+
+//update data products
+// localhost:8080/api/products/update?prodIdUp=9&value=title&data=rafa
+
+//add product
+// localhost:8080/api/products/add?title=carro&description=bla bla bla&price=2000&thumbnail=[{./img/img1.jpg},{./img/img2.jpg}]&stock=5&status=true&category=autos&code=4
+
+//delete product
+// localhost:8080/api/products/delete/1
+
+//get carts
+// localhost:8080/api/cart/
+
+//add product to cart
+// localhost:8080/api/cart/5/product/3
+
+//Crear carrito
+// localhost:8080/api/cart/
