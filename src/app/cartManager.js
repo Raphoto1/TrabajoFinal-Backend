27fafc8793cc3eeaa6183fe06536a9137e29bce2
Rafael Martinez <rrhhmmtt@gmail.com>
2023-03-02 22:33:47 -0500

    se logra filtrado de quanti estable

--- src/app/cartManager.js
+++ src/app/cartManager.js
@@ -20,7 +20,7 @@ class CartManager {
     carts = [...carts, newCart];
     await fs.promises.writeFile(this.#path, JSON.stringify(carts));
   }
-//organizador de ids
+  //organizador de ids
   async idOrganizer() {
     const carts = await this.getCarts();
     let chkCartNum = await carts.map((cart) => cart.cId);
@@ -62,19 +62,33 @@ class CartManager {
     const carts = await this.getCarts();
     const chkcId = await this.chkCartById(carts, cId);
     if (chkcId) {
-      const cartToWork = await carts.find(
-        (cart) => cart.cId === cId);
-      cartToWork.products = await [...cartToWork.products,{pId:pId, quantity:1}];
-      let updateCarts = await carts.filter(cart => cart.cId !== cId ); 
-      let updatedCarts = await [...updateCarts,cartToWork];
-      await fs.promises.writeFile(this.#path, JSON.stringify(updatedCarts));
-      return updatedCarts;
+      const cartToWork = await carts.find((cart) => cart.cId === cId);
+      //revisar si el producto ya existe en la lista
+      let prodInCart = cartToWork.products.some((p) => p.pId === pId);
+      // console.log(cartToWork.quantity=+1);
+      if (prodInCart) {
+        console.log("si existe el producto");
+        let updateQuanti = cartToWork.products.find((p) => p.pId === pId);
+        updateQuanti.quantity++
+        let updateCarts = await carts.filter((cart) => cart.cId !== cId);
+        let updatedCarts = [...updateCarts, cartToWork];
+        await fs.promises.writeFile(this.#path, JSON.stringify(updatedCarts));
+        return updatedCarts;
+      } else {
+        console.log("no existe el producto");
+        cartToWork.products = [
+          ...cartToWork.products,
+          { pId: pId, quantity: 1 },
+        ];
+        let updateCarts = await carts.filter((cart) => cart.cId !== cId);
+        let updatedCarts = [...updateCarts, cartToWork];
+        await fs.promises.writeFile(this.#path, JSON.stringify(updatedCarts));
+        return updatedCarts;
+      }
     } else {
       return console.log(`el carrito con el id ${cId} no existe`);
     }
   }
-
-
 }
 
 export default CartManager;
