a95036dd0c93d606d3000b58e5528154cc71ac42
Rafael Martinez <rrhhmmtt@gmail.com>
2023-03-02 20:05:42 -0500

    se crea estable createCart

--- src/app/cartManager.js
+++ src/app/cartManager.js
@@ -1,10 +1,110 @@
 import fs from "fs";
+import ProductManager from "./productManager.js";
+
+const item = new ProductManager();
 
 class CartManager {
-    #path = "./src/server/cart.json";
-    constructor(path){
-        path = this.#path;
+  #path = "./src/server/carts.json";
+  idAcum = 0;
+
+  constructor(path) {
+    path = this.#path;
+  }
+//creamos carrito
+async createCart(){
+  let carts = await this.getCarts();
+  const newCart = {
+    cId: await this.idOrganizer(),
+    products: [],
+  };
+  carts = [...carts, newCart];
+await fs.promises.writeFile(this.#path, JSON.stringify(carts));
+}
+
+  async idOrganizer() {
+    const carts = await this.getCarts();
+    let chkCartNum = await carts.map((cart) => cart.cId);
+    let highId = await Math.max(...chkCartNum);
+    if (highId === -Infinity) {
+      return 1;
+    } else {
+      console.log("pasa por adicion");
+      return highId + 1;
     }
+  }
+
+  async getCarts() {
+    try {
+      const prodsInCart = await fs.promises.readFile(this.#path, "utf-8");
+      return JSON.parse(prodsInCart);
+    } catch (error) {
+      return [];
+    }
+  }
+
+  async chkCartById(arr, cId) {
+    const chkCartId = await arr.some((cart) => cart.cId === cId);
+    return chkCartId;
+  }
+
+  async cartOrganizer() {
+    let carts = await this.getCarts();
+    //agregar un if si no existe nada para inicializar
+    if (carts === []) {
+      const newCart = {
+        cId: 1,
+        products: [],
+      };
+      carts = [...carts, newCart];
+    await fs.promises.writeFile(this.#path, JSON.stringify(carts));
+    } else {
+      const highId = await this.idOrganizer();
+      const newCart = {
+        cId: highId,
+        products: [],
+      };
+      carts = [...carts, newCart];
+      await fs.promises.writeFile(this.#path, JSON.stringify(carts));  
+    }
+  }
+
+  async addProductToCart(cId, pId){
+
+  }
+
+//   async addProductToCart(cId, pId) {
+//     const carts = await this.getCarts();
+//     const chkCart = await this.chkCartById(carts, cId);
+//     if (!chkCart) {
+//       await this.cartOrganizer();
+//       let newCart = await this.getCarts();
+//       const findProduct = newCart.find((cart) => (card.cId = cId))
+//       // const findProduct = await newCart.findIndex((cart) => (cart.cId = 1));
+//       // if (findProduct) {
+//       //   newCart[findProduct].products = {
+//       //       id: pId,
+//       //       quantity: 1,
+//       //     };
+//       // }
+//       console.log(findProduct);
+// cartsUpdated = [...newCart]
+       
+//       await fs.promises.writeFile(this.#path, JSON.stringify(cartsUpdated));
+//       return findProduct;
+//     } else {
+//       console.log("otro lado");
+//       const cartToUpdate = carts.find((cart) =>
+//         (cart.cId = cId)
+//           ? (cart.products = {
+//               id: pId,
+//               quantity: 1,
+//             })
+//           : console.log("no existe el carrito cone se id")
+//       );
+//       console.log(cartToUpdate);
+//       const cartsToAdd = await this.getCarts();
+//     }
+//   }
 }
 
 export default CartManager;
--- src/routes/cartManager.router.js
+++ src/routes/cartManager.router.js
@@ -1,6 +1,29 @@
 import { Router, json } from "express";
-import cartManager from "../app/cartManager.js"
+import CartManager from "../app/cartManager.js";
 
 const cartManagerRouter = Router();
+const cart = new CartManager();
+
+cartManagerRouter.get("/", async (req, res) => {
+    const prodsInCart = await cart.getCarts();
+    await res.send(prodsInCart);
+})
+
+cartManagerRouter.get("/:cId", async (req,res) =>{
+    const prodId = await Number(req.params.cId);
+    const result = await cart.getCarts(prodId)
+})
+
+cartManagerRouter.post("/", async (req,res) =>{
+ const newCart = await cart.createCart();
+ await res.send(newCart);
+})
+
+cartManagerRouter.post("/:cId/product/:pId", async (req,res) =>{
+    const cartId = await Number(req.params.cId);
+    const prodId = await Number(req.params.pId);
+    const result = await cart.addProductToCart(cartId, prodId);
+    await res.send(result);
+})
 
 export default cartManagerRouter;
