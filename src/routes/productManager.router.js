49a29c7842c1e2690d4bd604ffa34cc1fff27ee4
Rafael Martinez <rrhhmmtt@gmail.com>
2023-03-23 15:54:01 -0500

    se cruza con las correcciones anteriores

--- src/routes/productManager.router.js
+++ src/routes/productManager.router.js
@@ -6,67 +6,90 @@ productManagerRouter.use(json());
 const item = new ProductManager();
 
 
-productManagerRouter.get("/", async (req,res) => {
-    const {limit} = req.query;
-    const prods = await item.getProducts(); 
-    if(!limit){
-       await res.send(prods);
-    }else{
+productManagerRouter.get("/", async (req, res) => {
+    try {
+      const { limit } = req.query;
+      const prods = await item.getProducts();
+      if (!limit) {
+        res.send(prods);
+      } else {
         //envia el filtrado de el numero de datos
-    const filtered = prods.splice(0,limit);
-    await res.send(filtered);
-    }
-    
-});
+        const filtered = await prods.splice(0, limit);
+        res.send(filtered);
+      }
+    } catch (error) {}
+  });
 
 //segun el id
-productManagerRouter.get("/:id", async (req,res) => {
-    const prodId = await Number(req.params.id);
-    const log = console.log(prodId);
-    const result = await item.getProductById(prodId);
-    await res.send(result);
-})
+productManagerRouter.get("/:id", async (req, res) => {
+    try {
+      const prodId = Number(req.params.id);
+      const log = console.log(prodId);
+      const result = await item.getProductById(prodId);
+      return res.send(result);
+    } catch (err) {
+      res.status(404).send({error: `${err}`})
+    }
+  });
 
 //post carga de info code,
 
-productManagerRouter.post("/addTest", async(req, res) => {
-    const code = Number(req.body.code);
-    const title = req.body.title;
-    const test = console.log(code, title);
-    await res.send(test)
-})
+productManagerRouter.post("/", async (req, res, midSocket) => {
+    try {
+      const code = req.body.code;
+      const title = req.body.title;
+      const description = req.body.description;
+      const price = Number(req.body.price);
+      const thumbnail = req.body.thumbnail;
+      const stock = Number(req.body.stock);
+      const status = req.body.status;
+      const category = req.body.category;
+      const test = console.log(
+        code + title + description + price + stock + category
+      );
+      const result = await item.addProduct(
+        code,
+        title,
+        description,
+        price,
+        // thumbnail,
+        stock,
+        // status,
+        category
+      );
+      const enviarProds = await item.getProducts();
+      req.enviarProds = enviarProds;
+      midSocket();
+      res.send(result);
 
-productManagerRouter.post("/add", async(req, res, midSocket) => {
-    const code = Number(req.body.code);
-    const title = await req.body.title;
-    const description = await req.body.description;
-    const price = await Number(req.body.price);
-    // const thumbnail = await req.query.thumbnail;
-    const stock = await Number(req.body.stock);
-    // const status = await req.query.status;
-    const category = await req.body.category;
-    const test = console.log(code+title+description+price
-        +stock+category);
-    const result = await item.addProduct(code,title,description,price,stock,category);
-    const enviarProds = await item.getProducts()
-    req.enviarProds = enviarProds;
-    midSocket();
-    await res.send(test)
-})
+    } catch (err) {
+      res.status(404).send({error: `${err}`})
+    }
+  });
+//update
 
-productManagerRouter.post("/update", async (req,res) => {
-    const prodIdUp = await Number(req.query.prodIdUp);
-    const value = await req.query.value;
-    const data = await req.query.data;
-    // const result = prodIdUp + value + data;
-    const result = await item.updateProdById(prodIdUp,value,data);
-    await res.send(result);
-})
+productManagerRouter.put("/", async (req, res) => {
+    try {
+      const prodIdUp = Number(req.body.prodIdUp);
+      const value = req.body.value;
+      const data = req.body.data;
+      // const result = prodIdUp + value + data;
+      const result = await item.updateProdById(prodIdUp, value, data);
+      res.send(result);
+    } catch (err) {
+      res.status(404).send({error: `${err}`})
+    }
+  });
 
-productManagerRouter.get("/delete/:id", async (req,res) =>{
-    const prodId = await Number(req.params.id);
-    const result = await item.deleteProdById(prodId);
-    await res.send(result);
-})
+  //delete
+  productManagerRouter.delete("/:id", async (req, res) => {
+    try {
+      const prodId = Number(req.params.id);
+      const result = await item.deleteProdById(prodId);
+      res.send(result);
+    } catch (err) {
+      res.status(404).send({error: `${err}`})
+    }
+  });
 
 export default productManagerRouter;
