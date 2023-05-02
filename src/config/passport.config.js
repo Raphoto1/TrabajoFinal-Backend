0903c07f8be9af65e64a1e296e05c67ad25d7b1d
Rafael Martinez <rrhhmmtt@gmail.com>
2023-05-02 14:58:59 -0500

    se agregan datos a la db

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,10 +1,17 @@
 import passport from "passport";
 import LocalStrategy from "passport-local";
 import GithubStrategy from "passport-github2";
+import jwt, { ExtractJwt } from "passport-jwt";
 import { UserModel } from "../dao/models/user.model.js";
 import { createHash } from "../server/utils.js";
+import {options} from "../config/options.js";
+import { CartManager } from "../dao/index.js";
+
+const jwtStrategy = jwt.Strategy;
+const cartToWork = new CartManager();
 
 const initializedPassport = () => {
+  //local
   passport.use(
     "signupStrategy",
     new LocalStrategy(
@@ -12,16 +19,19 @@ const initializedPassport = () => {
         usernameField: "email",
         passreqToCallback: true,
       },
-      async ( username, password, done) => {
+      async (username, password, done) => {
         try {
           const user = await UserModel.findOne({ email: username });
           if (user) {
-            console.log("puede que ya estes registrado, si la falla persiste comunicate con admin");
+            console.log(
+              "puede que ya estes registrado, si la falla persiste comunicate con admin"
+            );
             return done(null, false);
           }
           const newUser = {
             email: username,
             password: createHash(password),
+            cart: await cartToWork.createCart()
           };
           const userCreated = await UserModel.create(newUser);
           return done(null, userCreated);
@@ -62,6 +72,22 @@ const initializedPassport = () => {
       }
     )
   );
+  
+  //jwt
+  passport.use("authJWT", new jwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
+      secretOrKey: "tokenFuerte"
+    },
+    async(jwt_payload,done) =>{
+      try {
+        return done(null,jwt_payload)
+      } catch (error) {
+        return done(error);
+      }
+    }
+    ))
+
   //serial
   passport.serializeUser((user, done) => {
     done(null, user._id);
@@ -73,4 +99,12 @@ const initializedPassport = () => {
   });
 };
 
-export { initializedPassport };
+const cookieExtractor = (req) => {
+  let token = null;
+  if(req && req.cookies){
+    token = req.cookies[options.server.cookieToken]
+  }
+  return token;
+}
+
+export { initializedPassport, cookieExtractor };
